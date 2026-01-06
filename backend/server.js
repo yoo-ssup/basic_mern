eed776272c58f840c196c5c9c659bc921aaa48f2
Vishwas <123109949+yoo-ssup@users.noreply.github.com>
2026-01-07 01:55:38 +0530

    ''

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const colors = require("colors");
-const { errorHandler } = require("./middleware/errorMiddleware");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 5000;
 
 const app = express();
